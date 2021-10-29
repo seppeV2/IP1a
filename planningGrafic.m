438dac2a34c19da6a23f3fd5f4f6d24f36b3ed1e
seppeV2 <seppe.vilain@student.kuleuven.be>
2021-10-29 21:14:16 +0200

    First step graphic schedual
    Also made a .gitignore for the *.m~ matlab files

---
+++ planningGrafic.m
@@ -0,0 +1,23 @@
+function times = planningGrafic()
+
+lindoRes = transformLindoData('lindoResults.txt'); %load the struct in 
+
+values = struct2cell(lindoRes); %get all the value's out of the struct
+disp(values);
+names = fieldnames(lindoRes); %get all the name's out of the struct
+
+for i=1:length(names)
+    
+    if (names{i}(1) == 'D') || (names{i}(1) == 'A')  % safe the starting times
+        train = extractBetween(names{i},2,length(names{i})-2);
+        if values{i}(1) > 60
+            times.(train{1}).(names{i}) = (values{i}(1)-60);
+        else
+           times.(train{1}).(names{i}) = values{i}(1);
+        end
+    end
+    
+    
+end
+end
+
