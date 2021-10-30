018fd113254f68a1148afab7d246ddb6ebf2e3cd
seppeV2 <seppe.vilain@student.kuleuven.be>
2021-10-31 01:58:51 +0200

    Added the plot part this is for now done
    maybe tis needs some adjustment when we watch it togheter

--- planningGrafic.m
+++ planningGrafic.m
@@ -1,4 +1,4 @@
-function timing = planningGrafic()
+function  timing = planningGrafic()
 
 
 
@@ -6,7 +6,7 @@ function timing = planningGrafic()
 lindoRes = transformLindoData('lindoResults.txt'); %load the struct in 
 
 values = struct2cell(lindoRes); %get all the value's out of the struct
-disp(values);
+
 names = fieldnames(lindoRes); %get all the name's out of the struct
 
 trains = zeros(0,0);
@@ -44,7 +44,79 @@ for i=1:length(names)
         end
    end
 end
-%% this section is to make the actual graph
+%% this section is to make the actual graph 
+% only plotting the trains 1 trains as the 2 trains don't have enough info
+
+%to plot everything every station has an id code made here in a struct
+stationID.LE = 1;
+stationID.LA = 2;
+stationID.ST = 4;
+stationID.AL = 6;
+stationID.HA = 7;
+stationID.AA = 3;
+stationID.HE = 5;
+
+close all 
+figure
+
+trains = fieldnames(timing);
+for i=1:length(trains)
+   %if (trains{i}(2) == '1')
+       stations = fieldnames(timing.(trains{i}));
+       times = zeros(0,0);
+       for j=1:length(stations)
+           %or two fields for a station (when it is passing) or one field
+           %for each station when it is or departure or arrival
+           
+           actions = fieldnames(timing.(trains{i}).(stations{j}));
+           
+           
+           if (length(actions) == 2)
+               if actions{1}(1) == 'S'  
+                times = [times ; [stationID.(stations{j}), timing.(trains{i}).(stations{j}).(actions{2})]];
+                times = [times ; [stationID.(stations{j}), (timing.(trains{i}).(stations{j}).(actions{2})-timing.(trains{i}).(stations{j}).(actions{1}))]];
+               else
+                times = [times ; [stationID.(stations{j}), timing.(trains{i}).(stations{j}).(actions{1})]];
+                times = [times ; [stationID.(stations{j}), (timing.(trains{i}).(stations{j}).(actions{1})-timing.(trains{i}).(stations{j}).(actions{2}))]];
+               end
+            
+               %some stations of the second train don't have a D-time or an
+               %A-time so these only have S-times so don't add these
+               %stations (this is for the next hour)
+           elseif (length(actions) == 1) && actions{1}(1) ~= 'S'
+               times = [times ; [stationID.(stations{j}), timing.(trains{i}).(stations{j}).(actions{1})]];
+           end
+         
+               
+       end
+       
+        times = sortrows(times,2); %so the plot is in the right order
+        disp(trains{i});
+        disp(times);
+      
+        
+        amount = size(times);
+        colors = 'rgbcmyk';
+        choice = randi(length(colors));
+        
+        if amount(1,1) ~= 1 %remove useless trains 
+            plot( times(:,2), times(:,1), '-*', 'DisplayName', trains{i}, 'LineWidth',1.5, 'MarkerEdgeColor', colors(choice));
+           
+
+            %to make the y axis readable
+            y_labels = {'Leuven';'Landen';'Aarschot';'Sint-Truiden';'Heist';'Alken';'Hasselt'};
+            y_values = [1;2;3;4;5;6;7];
+            set(gca, 'Ytick',y_values,'YTickLabel',y_labels);
+            
+            text(times(1,2),times(1,1), 'D' , 'FontSize', 15);
+            text(times(length(times),2),times(length(times),1), 'A' , 'FontSize', 15);
+
+            legend('FontSize',12);
+            hold all
+        end
+% end
+  
+end
 
 
 
